125d53e5f2fbaabe8679b7963009c11f217bdb32
Anonymouskb17 <mancity452k3@gmail.com>
2025-02-23 13:17:27 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import BodySection from "./components/BodySection";
+import Feature from "./components/Feature";
+import WorkFlow from "./components/WorkFlow";
+import Price from "./components/Price";
+import TestDetail from "./components/TestDetail";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <BodySection />
+        <Feature />
+        <WorkFlow />
+        <Price />
+        <TestDetail />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
